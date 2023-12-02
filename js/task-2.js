7705f57e2d28aff08b3e9e16008b438fe4fd2de0
yuliia11111992 <YuliiaAntoniukja@gmail.com>
2023-12-02 13:34:47 +0200

    add

---
+++ js/task-2.js
@@ -0,0 +1,12 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  const array3 = firstArray.concat(secondArray);
+  array3.length = maxLength;
+  return array3;
+}
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+
---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function filterArray(numbers, value){
+  const newNumber=[];
+  for (const number of numbers){
+    if (number>value){
+        newNumber.push(number);
+    }
+  }
+  return newNumber
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
